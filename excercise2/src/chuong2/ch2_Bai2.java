adb29b890537e85ff3869f2aa8f02132b94a54b3
VuNgocTuyenK2 <98011851+VuNgocTuyenK2@users.noreply.github.com>
2022-01-25 08:39:33 +0700

    first commit

---
+++ excercise2/src/chuong2/ch2_Bai2.java
@@ -0,0 +1,17 @@
+package chuong2;
+
+import java.util.Scanner;
+
+public class ch2_Bai2 {
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Kiểm tra chẵn lẻ");
+		System.out.print("Nhập n =  ");
+		int n = input.nextInt();
+		if (n % 2 == 0) {
+			System.out.println("N = " + n + " Là số chẵn");
+		} else {
+			System.out.println("N = " + n + " Là số lẻ");
+		}
+	}
+}
